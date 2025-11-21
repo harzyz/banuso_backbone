d3802cb553d489b08d2f96d058a2cc4ec83ea2e8
harzyz <abdulahmedazeez5@gmail.com>
2025-11-22 00:59:49 +0100

    update cors to use PATCH

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
